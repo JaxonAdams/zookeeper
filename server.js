0b20b3c22611f8e35e3d5864b6dcb6ea952fb5c2
Jaxon Adams <jadamsresume1@gmail.com>
2022-03-18 16:37:21 -0600

    added path to animals.html

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// instruct server to provide js and css files
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -51,24 +53,20 @@ function findById(id, animalsArray) {
 }
 
 function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name!== 'string') {
-        return false;
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
     }
-
     if (!animal.species || typeof animal.species !== 'string') {
-        return false;
+      return false;
     }
-
-    if (!animal.diet || typeof animal.tiet !== 'string') {
-        return false;
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
     }
-
     if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false;
+      return false;
     }
-
     return true;
-}
+  }
 
 function createNewAnimal(body, animalsArray) {
     const animal = body;
@@ -109,17 +107,24 @@ app.get('/api/animals/:id', (req, res) => {
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
-
+  
     // if any data in req.body is incorrect, send 400 error back
     if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
+      res.status(400).send('The animal is not properly formatted.');
     } else {
-        // add animal to json file and animals array in this function
-        const animal = createNewAnimal(req.body, animals);
-
-        res.json(animal);
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
     }
 });
+
+// index.html routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});  
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
